c5d358fbb5fece3fd1fd49757acbb82e09944f33
xordox <coolprabin2000@gmail.com>
2025-01-18 17:27:45 +0545

    configured and initialized hive

--- lib/features/trips/data/models/trip_model.dart
+++ lib/features/trips/data/models/trip_model.dart
@@ -1,10 +1,19 @@
 import 'package:travel_app/features/trips/domain/entities/trip.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+part 'trip_model.g.dart';
+
+@HiveType(typeId:0)
 class TripModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final List<String> photos;
+  @HiveField(2)
   final String description;
+  @HiveField(3)
   final DateTime date;
+  @HiveField(4)
   final String location;
 
   TripModel(
