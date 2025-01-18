88d316b5d2a89f1b0294b56cf47c00411b35e3da
xordox <coolprabin2000@gmail.com>
2025-01-18 16:52:35 +0545

    corrected mistake

--- lib/features/trips/presentation/pages/main_screen.dart
+++ lib/features/trips/presentation/pages/main_screen.dart
@@ -1,14 +1,1 @@
-class Trip {
-  final String title;
-  final List<String> photos;
-  final String description;
-  final DateTime date;
-  final String location;
 
-  Trip(
-      {required this.title,
-      required this.photos,
-      required this.description,
-      required this.date,
-      required this.location});
-}
